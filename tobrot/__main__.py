2e7749710fb1293b5b4a3bf38887d85217b99c66
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-05-04 18:20:16 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -14,6 +14,7 @@ from pyrogram.handlers import CallbackQueryHandler, MessageHandler
 from tobrot import (
     API_HASH,
     APP_ID,
+    BOT,
     AUTH_CHANNEL,
     CANCEL_COMMAND_G,
     CLEAR_THUMBNAIL,
@@ -216,13 +217,13 @@ if __name__ == "__main__":
     #
     upload_as_doc_handler = MessageHandler(
         upload_as_doc,
-        filters=filters.command(os.environ.get("TOGGLE_DOC", "toggledoc")) & filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command(os.environ.get("TOGGLE_DOC", "toggledoc")+BOT) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_doc_handler)
     #
     upload_as_video_handler = MessageHandler(
         upload_as_video,
-        filters=filters.command(os.environ.get("TOGGLE_VIDEO", "togglevideo")) & filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command(os.environ.get("TOGGLE_VIDEO", "togglevideo")+BOT) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_video_handler)
     clear_thumb_nail_handler = MessageHandler(
