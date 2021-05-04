6635329ec1b43332af4c9d890916c9ce46d8acc1
mewhere <83624712+mewhere@users.noreply.github.com>
2021-05-04 08:55:17 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -9,7 +9,11 @@ from logging.handlers import RotatingFileHandler
 from collections import defaultdict
 from sys import exit
 
-import dotenv
+if bool(os.environ.get("ENV", False)):
+    from tobrot.sample_config import Config
+else:
+    from tobrot.config import Config
+#from logging.handlers import RotatingFileHandler
 
 if os.path.exists("TorrentLeech-Gdrive.txt"):
     with open("Torrentleech-Gdrive.txt", "r+") as f_d:
@@ -33,7 +37,7 @@ logging.getLogger("PIL").setLevel(logging.WARNING)
 
 LOGGER = logging.getLogger(__name__)
 
-dotenv.load_dotenv("config.env")
+#dotenv.load_dotenv("config.env")
 
 # checking compulsory variable
 for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
