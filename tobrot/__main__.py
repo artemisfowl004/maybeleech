0f102957e71358569c5a1b88e264cc3e32b239c0
mewhere <83624712+mewhere@users.noreply.github.com>
2021-05-04 16:10:04 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -53,7 +53,8 @@ from tobrot.plugins.incoming_message_fn import (
     incoming_purge_message_f,
     incoming_youtube_dl_f,
     rename_tg_file,
-)
+    upload_as_doc, upload_as_video)
+#)
 from tobrot.plugins.new_join_fn import help_message_f, new_join_f
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
